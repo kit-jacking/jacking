cb8b53c6bdc49aebef78d0044caa8f6ca4c383b3
Tymon <tyren234@gmail.com>
2023-10-17 18:19:46 +0200

    Added graph class and accounted for it in main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from classes.node import Node
 from classes.edge import Edge
+from classes.graph import Graph
 from algorithms.algorithms import *
 
 A = Node("A", 0, 0)
@@ -9,28 +10,22 @@ D = Node("D", 1, 1)
 E = Node("E", 2, 1)
 F = Node("F", 2, 0)
 
-A.add_neighbours([Edge(A, B, 1), Edge(A, C, 2)])
-print(A)
+edges = [Edge(A, B, 1), Edge(A, C, 2), Edge(B, A, 1),
+         Edge(B, D, 2), Edge(C, A, 2), Edge(C, D, 2),
+         Edge(C, F, 6), Edge(D, B, 2), Edge(D, C, 2),
+         Edge(D, E, 2), Edge(E, D, 2), Edge(E, F, 2),
+         Edge(F, C, 6), Edge(F, E, 2)]
 
-B.add_neighbours([Edge(B, A, 1), Edge(B, D, 2)])
-print(B)
+nodes = [A, B, C, D, E, F]
 
-C.add_neighbours([Edge(C, A, 2), Edge(C, D, 2), Edge(C, F, 6)])
-print(C)
+graph = Graph(nodes, edges)
 
-D.add_neighbours([Edge(D, B, 2), Edge(D, C, 2), Edge(D, E, 2)])
-print(D)
-
-E.add_neighbours([Edge(E, D, 2), Edge(E, F, 2)])
-print(E)
-
-F.add_neighbours([Edge(F, C, 6), Edge(F, E, 2)])
 print(F)
 
 if __name__ == '__main__':
     print()
-    finish_node = F
-    start_node = A
+    start_node = graph.nodes[graph.nodes.index(A)]
+    finish_node = graph.nodes[graph.nodes.index(F)]
 
 
     def distance(node: Node) -> float:
