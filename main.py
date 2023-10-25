3062443f7ece116a62bea80eaf3c3f3515ecb394
iwitczak <igor.witczak@outlook.com>
2023-10-25 16:17:24 +0200

    Dijkstra algorithm final implementation

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from algorithms.algorithms import *
+from algorithms.dijkstra import dijkstra
 from example_graphs import example_graph_1, example_graph_halinow
 
 # graph, start_node, finish_node = example_graph_1()
@@ -8,9 +9,7 @@ if __name__ == '__main__':
     def distance(node: Node) -> float:
         return distance_between_nodes(node, finish_node)
 
-    output = a_star(start_node, finish_node, distance)
+    # output = a_star(start_node, finish_node, distance)
+    output = dijkstra(start_node, finish_node)
 
-    print("Found path to:")
-    print(output)
-    print("The path being:")
-    print(output.path())
+    print(f"Found path to: {output}\nPath:\n{output.path()}\nCost: {output.g}")
