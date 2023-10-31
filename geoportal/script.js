82c200161cbc04d1cdf00285a961ec6b23f2b2b2
Kuba <maciazek.jakub@gmail.com>
2023-10-31 18:12:04 +0100

    Podstawowe wyświetlanie mapy

---
+++ geoportal/script.js
@@ -0,0 +1,6 @@
+var map = L.map('map').setView([52, 19], 6);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
