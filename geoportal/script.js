dba108fb75d91c9b0e9cd2b33e89f2eb2637b6d7
Kuba <maciazek.jakub@gmail.com>
2023-11-03 21:43:44 +0100

    obsługa mapy

--- geoportal/script.js
+++ geoportal/script.js
@@ -1,6 +1,13 @@
-var map = L.map('map').setView([52, 19], 15);
+var map = L.map('map').setView([52.225, 21.357], 15);
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+
+
+var imageUrl = 'mapa.png';
+var altText = 'Mapa Halinowa';
+var latLngBounds = L.latLngBounds([[52.236589738, 21.376944963], [52.213842532, 21.336124121]]);
+
+var imageOverlay = L.imageOverlay(imageUrl, latLngBounds, {
+    opacity: 1,
+    alt: altText,
+    interactive: true
 }).addTo(map);
