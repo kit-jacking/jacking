273d37716ae4c1f6e19da6a3aa5a84db6312baef
iwitczak <igor.witczak@outlook.com>
2023-11-24 18:44:55 +0100

    Moved spatial indexing to flask server start

--- geoportal/geocode.py
+++ geoportal/geocode.py
@@ -2,17 +2,29 @@ from classes.node import Node
 from classes.graph import Graph
 from algorithms.algorithms import *
 from algorithms.a_star import *
-
+from example_graphs import *
 
 from flask import Flask, render_template, request, json
 import requests
 from requests.structures import CaseInsensitiveDict
 from urllib import parse
-from example_graphs import *
 from rtree import index
 from pyproj import transform, Proj
 
 app = Flask(__name__)
+
+# Load graphs and create spatial index
+print('Indexing Halinow...')
+graph_halinow, gdf_halinow, node_start_halinow, node_end_halinow = example_graph_shapefile(r'C:\Users\qattr\Desktop\CODE\GitHub\PAG2\jacking\shapefiles\Halinow Highways\Halinow Highways Latane.shp')
+idx_halinow = index.Index()
+for i, node in enumerate(graph_halinow.nodes):
+    idx_halinow.insert(i, (node.x, node.y, node.x, node.y), Node)
+print('Indexing Mazury...')    
+graph_mazury, gdf_mazury, node_start_mazury, node_end_mazury = example_graph_shapefile(r'C:\Users\qattr\Desktop\CODE\GitHub\PAG2\jacking\shapefiles\Mazury\PL.PZGiK.341.2806__OT_SKDR_L.shp')
+idx_mazury = index.Index()
+for i, node in enumerate(graph_mazury.nodes):
+    idx_mazury.insert(i, (node.x, node.y, node.x, node.y), Node)
+print('Finished')
 
 # Default action when webpage is opened - return html file
 @app.route('/')
@@ -35,6 +47,15 @@ def polska():
 @app.route('/getNodesFromAddress', methods=['GET', 'POST'])
 def getNodesFromAddress():
     api_key = request.form.get('APIKey')
+    region = request.form.get('mode')
+    
+    if region == 0:
+        nav_graph = graph_halinow
+        gdf = gdf_halinow
+    elif region == 1:
+        nav_graph = graph_mazury
+        gdf = gdf_mazury
+
     # Parse Address From from input box to URL format
     address_from = parse.quote(request.form.get('addressFrom'))
     url_from = f"https://api.geoapify.com/v1/geocode/search?text={address_from}&country=Poland&apiKey={api_key}"
@@ -54,7 +75,6 @@ def getNodesFromAddress():
     address_to = parse.quote(request.form.get('addressTo'))
     url_to = f"https://api.geoapify.com/v1/geocode/search?text={address_to}&country=Poland&apiKey={api_key}"
     
-    
     headers = CaseInsensitiveDict()
     headers["Accept"] = "application/json"
 
@@ -70,15 +90,8 @@ def getNodesFromAddress():
     in_proj = Proj(init = 'EPSG:4326')
     out_proj = Proj(init = 'EPSG:2180')
     
-    nav_graph, gdf, node_start, node_end = example_graph_shapefile(r'path')
-    
     tr_lon_from, tr_lat_from = transform(in_proj, out_proj, lon_from, lat_from)
     tr_lon_to, tr_lat_to = transform(in_proj, out_proj, lon_to, lat_to)
-    
-    print('\n\n\n\n')
-    print(node_start, node_end)
-    print(tr_lon_from, tr_lat_from)
-    print(tr_lon_to, tr_lat_to)
     
     # Nearest node search
     idx = index.Index()
